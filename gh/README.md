c22cf4e73ca9d59fa5c54d8e997262079eef5af1
Ali AKCA <ali@akca.io>
2023-10-28 01:24:17 +0200

    refactor(gh): bump module to dagger version 0.9.1

---
+++ gh/README.md
@@ -0,0 +1,38 @@
+# Dagger GH module
+
+![dagger-min-version](https://img.shields.io/badge/dagger%20version-v0.9.1-green)
+
+Dagger module for GitHub CLI.
+
+**Note:** This module is experimental and please use it with caution.
+
+## Prerequisites
+
+- KinD module requires Dagger CLI version `v0.9.1` or higher.
+
+## Before you start
+
+Set `DAGGER_MODULE` to environment variable to avoid using `-m github.com/aweris/daggerverse/gh` in every command.
+
+```shell
+export DAGGER_MODULE=github.com/aweris/daggerverse/gh
+```
+
+## Commands
+
+### Get Github CLI Binary
+
+```shell
+dagger download gh --version vx.y.z --export-path ./gh
+```
+
+### Running Github CLI
+
+```shell
+dagger run gh --version vx.y.z --token <token> --cmd "search repos --followers 1 --json url"
+```
+
+## Flags
+
+- `--version`: Version of the GitHub CLI binary to download. Defaults to `v2.37.0`.
+- `--token`: GitHub token to use. It requires to run any `gh` command. 
--- gh/main.go
+++ gh/main.go
@@ -9,29 +9,45 @@ import (
 	"github.com/hashicorp/go-getter"
 )
 
+const (
+	DefaultCLIVersion = "v2.37.0"
+)
+
+// Gh is Github CLI module for Dagger
 type Gh struct{}
 
-func (m *Gh) Run(ctx context.Context, version, token string, args []string) (string, error) {
-	file, err := m.GetGithubCli(ctx, version)
+func (m *Gh) Run(
+	ctx context.Context,
+	// version of the Github CLI (default: v2.37.0)
+	version Optional[string],
+	// Github token
+	token *Secret,
+	// command to run
+	cmd string,
+) (string, error) {
+	file, err := m.Get(ctx, version)
 	if err != nil {
 		return "", err
 	}
 
-	args = append([]string{"/usr/local/bin/gh"}, args...)
-
 	return dag.Container().
 		From("alpine/git:latest").
 		WithFile("/usr/local/bin/gh", file).
-		WithEnvVariable("GITHUB_TOKEN", token).
-		WithExec(args, ContainerWithExecOpts{SkipEntrypoint: true}).
+		WithSecretVariable("GITHUB_TOKEN", token).
+		WithExec([]string{"sh", "-c", strings.Join([]string{"/usr/local/bin/gh", cmd}, " ")}, ContainerWithExecOpts{SkipEntrypoint: true}).
 		Stdout(ctx)
 }
 
-func (m *Gh) GetGithubCli(ctx context.Context, version string) (*File, error) {
+// Get returns the Github CLI binary
+func (m *Gh) Get(
+	ctx context.Context,
+	// version of the Github CLI (default: v2.37.0)
+	version Optional[string],
+) (*File, error) {
 	var (
 		goos       = runtime.GOOS
 		goarch     = runtime.GOARCH
-		versionNum = strings.TrimPrefix(version, "v")
+		versionNum = strings.TrimPrefix(version.GetOr(DefaultCLIVersion), "v")
 	)
 
 	src := fmt.Sprintf("https://github.com/cli/cli/releases/download/v%s/gh_%s_%s_%s.tar.gz", versionNum, versionNum, goos, goarch)
