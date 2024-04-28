4aeb5370843749dc962fc6d074c12e3e430ad391
Ali AKCA <ali@akca.io>
2024-04-28 23:36:11 +0200

    refactor: extract container init from main gh struct

--- gh/main.go
+++ gh/main.go
@@ -13,62 +13,99 @@ type Gh struct {
 	// Configuration for the Github CLI binary
 	// +private
 	Binary GHBinary
+
+	// Configuration for the Github CLI container
+	// +private
+	Container GHContainer
 }
 
 func New(
-	// version of the Github CLI
+	// GitHub CLI version. (default: latest version)
 	// +optional
 	version string,
+
+	// GitHub token.
+	// +optional
+	token *Secret,
+
+	// GitHub repository (e.g. "owner/repo").
+	// +optional
+	repo string,
+
+	// Base container for the Github CLI
+	// +optional
+	base *Container,
 ) *Gh {
 	return &Gh{
 		Binary: GHBinary{
 			Version: version,
 		},
+		Container: GHContainer{
+			Base:  base,
+			Token: token,
+			Repo:  repo,
+		},
 	}
 }
 
+// Run a GitHub CLI command (accepts a single command string without "gh").
 func (m *Gh) Run(
 	ctx context.Context,
-	// Github token
-	token *Secret,
-	// command to run
+
+	// Command to run.
 	cmd string,
-	// version of the Github CLI
+
+	// GitHub CLI version. (default: latest version)
 	// +optional
-	// +default="2.37.0"
 	version string,
+
+	// GitHub token.
+	// +optional
+	token *Secret,
+
+	// GitHub repository (e.g. "owner/repo").
+	// +optional
+	repo string,
+
 	// disable cache
 	// +optional
 	// +default=false
 	disableCache bool,
-) (string, error) {
+) (*Container, error) {
 	file, err := lo.Ternary(version != "", m.Binary.WithVersion(version), m.Binary).binary(ctx)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	ctr := dag.Container().
-		From("alpine/git:latest").
-		WithFile("/usr/local/bin/gh", file).
-		WithSecretVariable("GITHUB_TOKEN", token)
+	// get the github container configuration
+	gc := m.Container
 
-	if disableCache {
-		ctr = ctr.WithEnvVariable("CACHEBUSTER", time.Now().String())
-	}
+	// update the container with the given token and repository if provided
+	gc = lo.Ternary(token != nil, gc.WithToken(token), gc)
+	gc = lo.Ternary(repo != "", gc.WithRepo(repo), gc)
 
-	return ctr.WithExec([]string{"sh", "-c", strings.Join([]string{"/usr/local/bin/gh", cmd}, " ")}, ContainerWithExecOpts{SkipEntrypoint: true}).
-		Stdout(ctx)
+	// get the container object with the given binary
+	ctr := gc.container(file)
+
+	// disable cache if flag is set
+	ctr = lo.Ternary(disableCache, ctr.WithEnvVariable("CACHE_BUSTER", time.Now().String()), ctr)
+
+	// run the command and return the container
+	return ctr.WithExec([]string{"sh", "-c", strings.Join([]string{"/usr/local/bin/gh", cmd}, " ")}, ContainerWithExecOpts{SkipEntrypoint: true}), nil
 }
 
-// Get returns the Github CLI binary
+// Get the GitHub CLI binary.
 func (m *Gh) Get(
 	ctx context.Context,
+
 	// operating system of the binary
 	// +optional
 	goos string,
+
 	// architecture of the binary
 	// +optional
 	goarch string,
+
 	// version of the Github CLI
 	// +optional
 	version string,
